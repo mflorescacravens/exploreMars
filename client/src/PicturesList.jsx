a57770e51cbf9650acc2d7ee5fd444818ad9d2a6
Guelito1 <mflorescacravens@gmail.com>
2020-03-04 21:06:43 -0800

    picture grid list fixed

--- client/src/PicturesList.jsx
+++ client/src/PicturesList.jsx
@@ -1,8 +1,38 @@
 import React, {useState, useEffect} from 'react'
 import './App.css';
 import axios from 'axios';
+import { makeStyles } from '@material-ui/core/styles';
+import GridList from '@material-ui/core/GridList';
+import GridListTile from '@material-ui/core/GridListTile';
+import GridListTileBar from '@material-ui/core/GridListTileBar';
+import IconButton from '@material-ui/core/IconButton';
+import StarBorderIcon from '@material-ui/icons/StarBorder';
+
+
+const useStyles = makeStyles(theme => ({
+    root: {
+        display: 'flex',
+        flexWrap: 'wrap',
+        justifyContent: 'space-around',
+        overflow: 'hidden',
+        backgroundColor: theme.palette.background.paper,
+    },
+    gridList: {
+        flexWrap: 'nowrap',
+        // Promote the list into his own layer on Chrome. This cost memory but helps keeping high FPS.
+        transform: 'translateZ(0)',
+    },
+    title: {
+        color: theme.palette.primary.light,
+    },
+    titleBar: {
+        background:
+            'linear-gradient(to top, rgba(0,0,0,0.7) 0%, rgba(0,0,0,0.3) 70%, rgba(0,0,0,0) 100%)',
+    },
+}));
 
 function PicturesList({rover, handleRoverChange, pageNum}) {
+    const classes = useStyles();
     const [pictures, setPictures] = useState([]);
 
     useEffect(() => {
@@ -13,15 +43,31 @@ function PicturesList({rover, handleRoverChange, pageNum}) {
 
     let content;
     if (pictures.length) {
-        content = pictures.map((picture, id) => {
-            return  <div>
-                        <img alt='roverPicture' 
-                            className='rover-pictures' 
-                            src={picture.img_src} 
-                            onClick={() => handleRoverChange(picture.img_src)} 
-                            key={id} />
-                    </div>
-        })
+        return  (
+            <div className={classes.root}>
+                <GridList className={classes.gridList} cols={2.5}>
+                    {pictures.map(function(picture, id) {
+                        return (
+                            <GridListTile key={id}>
+                                <img src={picture.img_src} alt={picture.img_src} />
+                                <GridListTileBar
+                                title={picture.img_src}
+                                classes={{
+                                    root: classes.titleBar,
+                                    title: classes.title,
+                                }}
+                                actionIcon={
+                                    <IconButton aria-label={`star ${picture.img_src}`}>
+                                    {/* <StarBorderIcon className={classes.title} /> */}
+                                    </IconButton>
+                                }
+                                />
+                            </GridListTile>
+                        )
+                    })}
+                </GridList>
+            </div>
+        )
         // content = <p>pictures are here</p>
 
     } else {
